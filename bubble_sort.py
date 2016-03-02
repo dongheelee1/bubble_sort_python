4977847ab978075639062624007a12d068feeb6f
Dong Hee Lee <dongheec.lee@gmail.com>
2016-03-01 16:24:56 -0800

    second commit

--- bubble_sort.py
+++ bubble_sort.py
@@ -18,25 +18,25 @@ for count1 in range (0, len(lst)):
 print lst	
 print "seconds: ", datetime.timedelta(seconds=1)  
 
-count1 = 0, <5
+# count1 = 0, <5
 
-count1 = 0
-	count2 = 0:
-	temp = 8
-	lst[0] = 3
-	lst[1] = 8, lst=[3, 8, 4, 2, 5]
+# count1 = 0
+# 	count2 = 0:
+# 	temp = 8
+# 	lst[0] = 3
+# 	lst[1] = 8, lst=[3, 8, 4, 2, 5]
 
-	count2 = 1:
-	temp = 8
-	lst[1] = 4
-	lst[2] = 8, lst=[3, 4, 8, 2, 5]
+# 	count2 = 1:
+# 	temp = 8
+# 	lst[1] = 4
+# 	lst[2] = 8, lst=[3, 4, 8, 2, 5]
 
-	count2 = 2:
-	temp = 8
-	lst[2] = 2
-	lst[3] = 8, lst=[3, 4, 2, 8, 5]
+# 	count2 = 2:
+# 	temp = 8
+# 	lst[2] = 2
+# 	lst[3] = 8, lst=[3, 4, 2, 8, 5]
 
-	count2 = 3:
-	temp = 8
-	lst[3] = 5
-	lst[4] = 8, lst=[3, 4, 2, 5, 8]
+# 	count2 = 3:
+# 	temp = 8
+# 	lst[3] = 5
+# 	lst[4] = 8, lst=[3, 4, 2, 5, 8]
